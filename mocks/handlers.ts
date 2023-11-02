fb81afdb8aa36c57a7eeb4bdbe77ba0b7c55d0b5
Anton Savoskin <sairu2k@gmail.com>
2023-11-02 11:31:06 +0600

    Reproduce the error

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -2,10 +2,12 @@ import { http, graphql, HttpResponse } from 'msw'
 
 export const handlers = [
   http.get('https://api.example.com/user', () => {
-    return HttpResponse.json({
+    const headers = new Headers()
+    headers.append('Content-Type', 'application/json');
+    return new Response(JSON.stringify({
       firstName: 'John',
       lastName: 'Maverick',
-    })
+    }), {headers, status: 200, statusText: 'OK'})
   }),
   graphql.query('ListMovies', () => {
     return HttpResponse.json({
