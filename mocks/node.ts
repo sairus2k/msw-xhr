53f83cbd1cc2c073bfd47f64d9aecc759ab538e5
Anton Savoskin <sairu2k@gmail.com>
2023-11-10 10:20:38 +0600

    Update msw, move mocks to vitest.setup.ts

--- mocks/node.ts
+++ mocks/node.ts
@@ -1,19 +1,3 @@
-import XMLHttpRequest from './xhr.js'
-import {fetch} from 'whatwg-fetch'
-// @ts-ignore
-import Response from 'react-native-fetch-api/src/Response';
-// @ts-ignore
-import Request from 'react-native-fetch-api/src/Request';
-// @ts-ignore
-import Headers from 'react-native-fetch-api/src/Headers';
-global.XMLHttpRequest = XMLHttpRequest
-global.fetch = fetch
-global.Request = Request
-global.Response = Response
-global.Headers = Headers
-// @ts-ignore
-global.location = {href: 'https://api.example.com'};
-
 import { setupServer } from 'msw/native'
 import { handlers } from './handlers.js'
 
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,3 +1,17 @@
+import XMLHttpRequest from './mocks/xhr.js'
+import {fetch} from 'whatwg-fetch'
+// @ts-ignore
+import Response from 'react-native-fetch-api/src/Response';
+// @ts-ignore
+import Request from 'react-native-fetch-api/src/Request';
+// @ts-ignore
+import Headers from 'react-native-fetch-api/src/Headers';
+global.XMLHttpRequest = XMLHttpRequest
+global.fetch = fetch
+global.Request = Request
+global.Response = Response
+global.Headers = Headers
+
 import { server } from './mocks/node.js'
 
 beforeAll(() => {
